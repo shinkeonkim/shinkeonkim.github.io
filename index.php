9a63e08d976b6978a7c165bf6ffaf05a851d520a
shinkeonkim <singun11@naver.com>
2019-02-08 01:24:23 +0900

    php로 변경

--- index.php
+++ index.php
@@ -11,14 +11,17 @@
   </head>
   
   <body>
-    <br>
-    <img src="site_name.jpg" width="200" height="50"> 
+    <header class="site-header">
+      <a href="index.php">HM & KM Coder </a>
+      <!--<img src="site_name.jpg" width="200" height="50">  
+         <button> BOJ 문제 & 해설 </button>-->
+    </header>
     <h3 style="color: black"> 한민고등학교 && 국민대학교  </h3>
     <p>........공부한거 정리........(온라인 저지를 만들고 싶다..)</p>
     <p> 단지 HTML로 실험하는 commit 입니당</p>
+<!--
     아이디:   <input type="text" name=text1 size="20">
     비밀번호: <input type="password" name=psd size="20">
-<!--
     <br>
     <input type="radio" value="1">1
     <input type="radio" value="2">2
